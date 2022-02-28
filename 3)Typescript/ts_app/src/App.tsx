726320d270b24848b8b8f0d3ee821c6fcdec1272
jungmyungjin <myung2667@gmail.com>
2022-03-01 05:21:03 +0900

    Typescript 3.4 from 파일 분리

--- 3)Typescript/ts_app/src/App.tsx
+++ 3)Typescript/ts_app/src/App.tsx
@@ -1,34 +1,10 @@
 import React from "react";
 import { useState } from "react";
 // import Circle from "./Circle";
+// import Form from "./Form";
 
 function App() {
-  const [value, setValue] = useState("");
-  // Typescript에서는 가능한 타입을 명확히 설정해 주어야 한다.
-  // event와 같은 타입을 모르는 경우, document를 확인하거나 구글링을 해야한다.
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello " + value);
-  };
-  return (
-    <div className="App">
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>login</button>
-      </form>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- 3)Typescript/ts_app/src/Form.tsx
+++ 3)Typescript/ts_app/src/Form.tsx
@@ -1,5 +1,33 @@
-function form() {
-  return 0;
+import React from "react";
+import { useState } from "react";
+
+function Form() {
+  const [value, setValue] = useState("");
+  // Typescript에서는 가능한 타입을 명확히 설정해 주어야 한다.
+  // event와 같은 타입을 모르는 경우, document를 확인하거나 구글링을 해야한다.
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello " + value);
+  };
+  return (
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>login</button>
+      </form>
+    </div>
+  );
 }
 
-export default form;
+export default Form;
