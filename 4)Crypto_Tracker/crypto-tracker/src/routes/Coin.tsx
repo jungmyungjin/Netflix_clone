03f535189aa645f78ad65b5b93823ce45e600c99
jungmyungjin <myung2667@gmail.com>
2022-03-02 05:48:29 +0900

    Crypto Tracker 4.6 api를 사용하여 코인에 대한 정보를 가져오기

--- 4)Crypto_Tracker/crypto-tracker/src/routes/Coin.tsx
+++ 4)Crypto_Tracker/crypto-tracker/src/routes/Coin.tsx
@@ -1,5 +1,6 @@
-import { useParams } from "react-router";
+import { useParams, useLocation } from "react-router";
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 
 // type 정의 방법1
 interface Params {
@@ -8,12 +9,138 @@ interface Params {
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
 `;
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0px auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Loading = styled.span`
+  text-align: center;
+  display: block;
+  margin: 50px auto;
+`;
+
+interface RouteState {
+  name: string;
+}
 
 function Coin() {
   // react-router-dom v6가 되면서 사용법이 바뀌었다...
+  // useParams : 파라미터의 정보를 가져온다.
   const { coinId } = useParams() as unknown as Params;
-  return <Title>Coin {coinId}</Title>;
+  const [loading, setLoading] = useState(true);
+  // useLocation : react-router-dom 의 Link 에서 넘겨준 데이터를 받는다.
+  // 새로 api를 받지 않고 기존에 있는 데이터를 사용함으로써, 사용자가 보기에 속도가 빠르게 느껴진다.
+  const location = useLocation();
+  // 아래의 state는 home 에서 만들어진 state이므로, (크롬 시크릿모드에서)home을 거치지 않고 바로 코인주소를 치게되면 에러가 난다.
+  const state = location.state as RouteState;
+
+  interface ITag {
+    coin_counter: number;
+    ico_counter: number;
+    id: string;
+    name: string;
+  }
+
+  // 타입스크립트 코드 베이스에 보면 인터페이스명 앞에 'I'를 붙힌다
+  interface IInfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    // Array안에 타입이 또 있는경우 인터페이스를 따로 작성해주어야 한다.
+    tags: ITag[]; // Object라고 되어있으나, 실제로는 Object가 아니다
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+  }
+
+  interface IPriceDate {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+      USD: {
+        ath_date: string;
+        ath_price: number;
+        market_cap: number;
+        market_cap_change_24h: number;
+        percent_change_1h: number;
+        percent_change_1y: number;
+        percent_change_6h: number;
+        percent_change_7d: number;
+        percent_change_12h: number;
+        percent_change_15m: number;
+        percent_change_24h: number;
+        percent_change_30d: number;
+        percent_change_30m: number;
+        percent_from_price_ath: number;
+        price: number;
+        volume_24h: number;
+        volume_24h_change_24h: number;
+      };
+    };
+  }
+
+  const [info, setInfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceDate>();
+
+  interface IPriceData {}
+
+  useEffect(() => {
+    // 해당 코인에 대한 정보를 가져오는 api, 한번만 실행됨
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title> {state ? `코인 ${state.name}` : "Loading..."}</Title>
+      </Header>
+      {/* priceInfo? : typescript가 api에서 오는 것인걸 알고, 그렇다면 항상 있는 값이 아니므로 자동으로 붙여준다 */}
+      {loading ? (
+        <Loading>Loading....</Loading>
+      ) : (
+        `USD : ${priceInfo?.quotes.USD.price}`
+      )}
+    </Container>
+  );
 }
 
 export default Coin;
--- 4)Crypto_Tracker/crypto-tracker/src/routes/Coins.tsx
+++ 4)Crypto_Tracker/crypto-tracker/src/routes/Coins.tsx
@@ -1,5 +1,105 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0px auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    transition: color 0.2s ease-in;
+    display: flex;
+    align-items: center;
+    padding: 20px;
+  }
+  &:hover {
+    // Link를 사용했으나, a태그로 설정해도 적용 되는 이유는,
+    // 결국 reacr-router-link 들이 a태그로 바뀌기 때문이다.
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Loading = styled.span`
+  text-align: center;
+  display: block;
+  margin: 50px auto;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 function Coins() {
-  return <h1>Coins</h1>;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  // 한번만 실행
+  useEffect(() => {
+    // 함수를 바로 만들고 실행
+    // (()=>{}) ();
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+    })();
+    setLoading(false);
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {loading ? (
+        <Loading>Loading....</Loading>
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            // &rarr; : 화살표(->)
+            <Coin key={coin.id}>
+              {/* react-router-dom 의 state를 사용하여 정보를 넘겨준다 */}
+              <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                  alt=""
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;
