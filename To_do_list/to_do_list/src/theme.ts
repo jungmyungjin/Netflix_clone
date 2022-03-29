a74a5c0f13ad8dba3494a9efa333b5deedab007c
jungmyungjin <myung2667@gmail.com>
2022-03-30 01:43:16 +0900

    To do list : 기본적인 페이지 구현

---
+++ To_do_list/to_do_list/src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#032613",
+  textColor: "#020D05",
+  accentColor: "#035951",
+  subAccentColor: "#D9D9D9",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#F2F2F2",
+  textColor: "#1E2611",
+  accentColor: "#90A67B",
+  subAccentColor: "#A4A6A6",
+};
